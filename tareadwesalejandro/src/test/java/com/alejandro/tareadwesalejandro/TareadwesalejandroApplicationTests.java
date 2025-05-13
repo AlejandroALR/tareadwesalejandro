e83574ebfe0b450965fefc6027a8630b3d92b5cd
mdc24518 <mdc24518@educastur.es>
2025-05-13 12:35:14 +0200

    Primer commit

---
+++ tareadwesalejandro/src/test/java/com/alejandro/tareadwesalejandro/TareadwesalejandroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.tareadwesalejandro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareadwesalejandroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
